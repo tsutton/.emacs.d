6c6c9182a21060533fbd64ae3af60a01c88b6278
Taylor Sutton <tsutton125@gmail.com>
2021-03-06 16:44:07 -0800

    Add vterm, a terminal emulator inside of emacs.

--- init.el
+++ init.el
@@ -628,6 +628,23 @@
 (savehist-mode +1)
 
 
+(use-package vterm
+  :ensure t
+  :bind
+  ("C-x M-t" . vterm)
+  :config
+  ;; This code allows sets up message passing so that when you change directory inside vterm,
+  ;; emacs also updates your working directory (so that find-file starts from the right place, e.g.)
+  ;; It is required to also add some stuff to your shell config.
+  ;; see https://github.com/akermu/emacs-libvterm
+  ;; The recommended way seems to be modifying the prompt to print some message, but that didn't
+  ;; work for me, so instead I use this method.
+  (add-to-list 'vterm-eval-cmds '("update-pwd" (lambda (path) (setq default-directory path))))
+  )
+;; TODO Help with renaming vterms
+;; I almost always rename terminal buffers (using rename-buffer), typically to ${something}-terminal
+;; It would be nice to have some easy way to do this.
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom
 (custom-set-variables
